ce3dcf28c013228a2d9253c03ef8408766630c33
Nicky Croydon <nickc87@hotmail.co.uk>
2019-01-16 09:40:15 +0000

    Create landing route and setup express

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express"),
+      app = express();
+      
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+   res.render("landing_v1"); 
+});
+      
+      
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("RUNNING!!!");
+});
