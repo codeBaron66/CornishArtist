1819b7d90ae711736dd58fb48c4900aa644589c7
Nicky Croydon <nickc87@hotmail.co.uk>
2019-01-16 12:08:14 +0000

    Update Routes

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-   res.render("landing_v1"); 
+   res.render("landing"); 
 });
       
+app.get("/about", function(req, res){
+   res.render("about"); 
+});
+
+app.get("/gallery", function(req, res){
+   res.render("gallery"); 
+});
+
+app.get("/contact", function(req, res) {
+   res.render("contact"); 
+});
       
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("RUNNING!!!");
