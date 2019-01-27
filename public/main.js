c40f31b01fb782a7f6e2ad15161c949f081aeb85
Nicky Croydon <nickc87@hotmail.co.uk>
2019-01-27 16:02:33 +0000

    Complete landing page and make resposive

--- public/main.js
+++ public/main.js
@@ -1,3 +1,7 @@
 function toggleSidebar(){
   document.body.classList.toggle('sidebar-active');
 }
+// function hideSidebar(){
+//     document.body.classList.remove('sidebar-active');
+// }
+// // window.addEventListener('click', hideSidebar());
